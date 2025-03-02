dfbd6ea805b34216082e478e33e19c172a0b1ee3
oldsaltfish <3295696322@qq.com>
2025-03-02 22:58:29 +0800

    fix: 更换pnpm管理器

--- global.d.ts
+++ global.d.ts
@@ -1,19 +1,3 @@
-declare module 'https://cdn.jsdelivr.net/gh/lit/dist@3.1.2/core/lit-core.min.js' {
+declare module 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js' {
   export * from 'lit';
 }
-
-// import { LitElement, html, css } from 'lit';
-
-// declare global {
-//   const html: typeof import('lit').html;
-//   const css: typeof import('lit').css;
-//   const LitElement: typeof import('lit').LitElement;
-//   const lit : typeof import('lit')
-//   interface Window {
-//     lit: {
-//       html: typeof import('lit').html;
-//       css: typeof import('lit').css;
-//       LitElement: typeof import('lit').LitElement;
-//     }
-//   }
-// }
